fb005734f039500255794f86a2a0fbef5f7cda89
rmitsuboshi <ryotaro.mitsuboshi@gmail.com>
2021-10-19 11:53:54 +0900

    Implement dstump for sparse data (not tested yet, but compile is fine)

--- src/base_learner/dstump.rs
+++ src/base_learner/dstump.rs
@@ -1,6 +1,7 @@
 use crate::data_type::{DType, Data, Label, Sample};
 use crate::base_learner::core::BaseLearner;
 use crate::base_learner::core::Classifier;
+use std::collections::HashSet;
 
 
 
@@ -120,50 +121,122 @@ impl BaseLearner<f64, f64> for DStump {
         };
 
 
+        let mut update_params = |best_edge: &mut f64, edge: f64, threshold: f64, j: usize| {
+            if *best_edge < edge.abs() {
+                dstump_classifier.threshold = threshold;
+                dstump_classifier.feature_index = j;
+                *best_edge = edge.abs();
+                if edge > 0.0 {
+                    dstump_classifier.positive_side = PositiveSide::RHS;
+                } else {
+                    dstump_classifier.positive_side = PositiveSide::LHS;
+                }
+            }
+        };
 
         match sample.dtype {
             DType::Sparse => {
-                todo!();
+                for j in 0..self.feature_size {
+                    let mut edge = init_edge;
+
+                    let zero_value = {
+                        let mut _zero_values = 0.0;
+                        let _idx = self.indices[j].iter().collect::<HashSet<&usize>>();
+                        for _i in 0..self.sample_size {
+                            if !_idx.contains(&_i) {
+                                let (_, _label) = sample[_i];
+                                _zero_values += _label * distribution[_i];
+                            }
+                        }
+                        _zero_values
+                    };
+
+
+                    let mut left: f64;
+                    let mut right = sample[self.indices[j][0]].0.value_at(j);
+
+                    let mut idx = self.indices[j].iter().peekable();
+
+                    let mut still_negative = right < 0.0;
+                    while let Some(&i) = idx.next() {
+                        let (example, label) = &sample[i];
+
+
+                        if still_negative && example.value_at(i) > 0.0 {
+                            still_negative = false;
+                            left = right;
+                            right = 0.0;
+
+                            edge -= 2.0 * zero_value;
+
+                            let threshold = (left + right) / 2.0;
+                            update_params(&mut best_edge, edge, threshold, j);
+                        }
+
+                        edge -= 2.0 * distribution[i] * label;
+
+
+                        left = right;
+                        if let Some(&&i_next) = idx.peek() {
+                            let (next_example, _) = &sample[i_next];
+                            if right == next_example.value_at(j) {
+                                continue;
+                            }
+                            right = next_example.value_at(j);
+                        } else {
+                            right = example.value_at(j) + 1.0;
+                        }
+
+
+                        let threshold = (left + right) / 2.0;
+                        update_params(&mut best_edge, edge, threshold, j);
+                    }
+
+                    if still_negative {
+                        left = right;
+                        right = 0.0;
+
+                        edge -= 2.0 * zero_value;
+
+                        let threshold = (left + right) / 2.0;
+                        update_params(&mut best_edge, edge, threshold, j);
+                    }
+                }
             },
             DType::Dense => {
                 for j in 0..self.feature_size {
-                    let idx = &self.indices[j];
-
                     let mut edge = init_edge;
 
 
                     let mut left;
-                    let mut right = sample[idx[0]].0.value_at(j);
+                    let mut right = sample[self.indices[j][0]].0.value_at(j);
 
 
-                    for ii in 0..self.sample_size {
-                        let i = idx[ii];
+                    let mut idx = self.indices[j].iter().peekable();
 
+                    while let Some(&i) = idx.next() {
                         let (example, label) = &sample[i];
 
                         edge -= 2.0 * distribution[i] * label;
 
-                        if ii + 1_usize != self.sample_size && (right == sample[idx[ii+1]].0.value_at(j) || *label == sample[idx[ii+1]].1) { continue; }
-
-                        left  = right;
-                        right = if ii + 1_usize == self.sample_size { example.value_at(j) + 1.0 } else { sample[idx[ii+1]].0.value_at(j) };
-
-                        if best_edge < edge.abs() {
-                            dstump_classifier.threshold = (left + right) / 2.0;
-                            dstump_classifier.feature_index = j;
-                            if edge > 0.0 {
-                                best_edge  = edge;
-                                dstump_classifier.positive_side = PositiveSide::RHS;
-                            } else {
-                                best_edge  = - edge;
-                                dstump_classifier.positive_side = PositiveSide::LHS;
+                        left = right;
+                        if let Some(&&i_next) = idx.peek() {
+                            let (next_example, _) = &sample[i_next];
+                            if right == next_example.value_at(j) {
+                                continue;
                             }
+                            right = next_example.value_at(j);
+                        } else {
+                            right = example.value_at(j) + 1.0;
                         }
+
+                        let threshold = (left + right) / 2.0;
+                        update_params(&mut best_edge, edge, threshold, j);
                     }
                 }
-                Box::new(dstump_classifier)
             },
         }
+        Box::new(dstump_classifier)
     }
 }
 
