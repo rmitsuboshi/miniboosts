e8b3bd1d85dcea8e05ac7330b861d7c5a7655df7
rmitsuboshi <ryotaro.mitsuboshi@gmail.com>
2021-10-24 23:23:39 +0900

    Add doc string to Booster trait (only one line)

--- src/booster/core.rs
+++ src/booster/core.rs
@@ -2,6 +2,7 @@ use crate::data_type::{Data, Label, Sample};
 use crate::base_learner::core::Classifier;
 use crate::base_learner::core::BaseLearner;
 
+/// The trait `Booster` defines the standard framework of Boosting.
 pub trait Booster<D, L> {
 
     fn update_params(&mut self, h: Box<dyn Classifier<D, L>>, sample: &Sample<D, L>) -> Option<()>;
