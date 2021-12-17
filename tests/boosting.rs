a62280afe035fead58d4d218d5524e3e271b6529
rmitsuboshi <ryotaro.mitsuboshi@gmail.com>
2021-12-18 01:01:01 +0900

    Add tests for SoftBoost

--- tests/boosting.rs
+++ tests/boosting.rs
@@ -3,7 +3,7 @@ extern crate boost;
 use std::env;
 
 use boost::booster::Booster;
-use boost::booster::{AdaBoost, LPBoost, ERLPBoost};
+use boost::booster::{AdaBoost, LPBoost, ERLPBoost, SoftBoost};
 use boost::base_learner::DStump;
 
 use boost::data_reader::{read_libsvm, read_csv};
@@ -31,9 +31,9 @@ pub mod adaboost_tests {
 
 
         let mut loss = 0.0;
-        for i in 0..sample.len() {
-            let p = adaboost.predict(&sample[i].data);
-            if sample[i].label != p { loss += 1.0; }
+        for ex in sample.iter() {
+            let p = adaboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
         }
 
         loss /= sample.len() as f64;
@@ -60,9 +60,9 @@ pub mod adaboost_tests {
 
 
         let mut loss = 0.0;
-        for i in 0..sample.len() {
-            let p = adaboost.predict(&sample[i].data);
-            if sample[i].label != p { loss += 1.0; }
+        for ex in sample.iter() {
+            let p = adaboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
         }
 
         loss /= sample.len() as f64;
@@ -82,7 +82,8 @@ pub mod lpboost_tests {
         println!("path: {:?}", path);
         path.push("tests/small_toy_example.csv");
         let sample = read_csv(&path).unwrap();
-    
+
+
         let mut lpboost = LPBoost::init(&sample);
         let dstump = DStump::init(&sample);
         let dstump = Box::new(dstump);
@@ -92,9 +93,37 @@ pub mod lpboost_tests {
 
 
         let mut loss = 0.0;
-        for i in 0..sample.len() {
-            let p = lpboost.predict(&sample[i].data);
-            if sample[i].label != p { loss += 1.0; }
+        for ex in sample.iter() {
+            let p = lpboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - lpboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+
+    #[test]
+    fn run_softmargin() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
+        let sample = read_csv(&path).unwrap();
+
+        let cap = sample.len() as f64 * 0.2;
+    
+        let mut lpboost = LPBoost::init(&sample).capping(cap);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        lpboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for ex in sample.iter() {
+            let p = lpboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
         }
 
         loss /= sample.len() as f64;
@@ -121,9 +150,9 @@ pub mod lpboost_tests {
 
 
         let mut loss = 0.0;
-        for i in 0..sample.len() {
-            let p = lpboost.predict(&sample[i].data);
-            if sample[i].label != p { loss += 1.0; }
+        for ex in sample.iter() {
+            let p = lpboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
         }
 
         loss /= sample.len() as f64;
@@ -154,9 +183,39 @@ pub mod erlpboost_tests {
 
 
         let mut loss = 0.0;
-        for i in 0..sample.len() {
-            let p = erlpboost.predict(&sample[i].data);
-            if sample[i].label != p { loss += 1.0; }
+        for ex in sample.iter() {
+            let p = erlpboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - erlpboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+
+
+    #[test]
+    fn run_softmargin() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
+        let sample = read_csv(&path).unwrap();
+
+
+        let cap = sample.len() as f64 * 0.2;
+    
+        let mut erlpboost = ERLPBoost::init(&sample).capping(cap);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        erlpboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for ex in sample.iter() {
+            let p = erlpboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
         }
 
         loss /= sample.len() as f64;
@@ -183,9 +242,9 @@ pub mod erlpboost_tests {
 
 
         let mut loss = 0.0;
-        for i in 0..sample.len() {
-            let p = erlpboost.predict(&sample[i].data);
-            if sample[i].label != p { loss += 1.0; }
+        for ex in sample.iter() {
+            let p = erlpboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
         }
 
         loss /= sample.len() as f64;
@@ -195,3 +254,96 @@ pub mod erlpboost_tests {
 }
 
 
+
+
+/// Tests for `SoftBoost`.
+/// Since `TotalBoost` is implemented via `SoftBoost`,
+/// we do not need to test `TotalBoost`.
+#[cfg(test)]
+pub mod softboost_tests {
+    use super::*;
+    #[test]
+    fn run() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
+        let sample = read_csv(&path).unwrap();
+    
+        let mut softboost = SoftBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        softboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for ex in sample.iter() {
+            let p = softboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - softboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+
+
+    #[test]
+    fn run_softmargin() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
+        let sample = read_csv(&path).unwrap();
+
+
+        let cap = sample.len() as f64 * 0.2;
+    
+        let mut softboost = SoftBoost::init(&sample).capping(cap);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        softboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for ex in sample.iter() {
+            let p = softboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - softboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+
+
+    #[test]
+    fn run_with_libsvm() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example_libsvm.txt");
+        let sample = read_libsvm(path).unwrap();
+        println!("sample.len() is: {:?}, sample.feature_len() is: {:?}", sample.len(), sample.feature_len());
+
+
+        let mut softboost = SoftBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        softboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for ex in sample.iter() {
+            let p = softboost.predict(&ex.data);
+            if ex.label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - softboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+}
