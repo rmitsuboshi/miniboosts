c96a22d917d686cd2134ec141558b0f8950576eb
rmitsuboshi <ryotaro.mitsuboshi@gmail.com>
2021-12-18 00:25:45 +0900

    Append TotalBoost & SoftBoost

--- src/booster.rs
+++ src/booster.rs
@@ -3,9 +3,13 @@ pub mod adaboost;
 pub mod adaboostv;
 pub mod lpboost;
 pub mod erlpboost;
+pub mod softboost;
+pub mod totalboost;
 
 // Export the Boosters
 pub use self::core::Booster;
 pub use adaboost::AdaBoost;
 pub use lpboost::LPBoost;
 pub use erlpboost::ERLPBoost;
+pub use softboost::SoftBoost;
+pub use totalboost::TotalBoost;
