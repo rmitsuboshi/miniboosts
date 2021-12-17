897f89d0594ea5edcbb60216e563de1389553809
rmitsuboshi <ryotaro.mitsuboshi@gmail.com>
2021-12-17 16:30:01 +0900

    Minor updates.
    Rename files,
    Add grb to Cargo.toml

--- tests/boosting.rs
+++ tests/boosting.rs
@@ -1,87 +1,197 @@
 extern crate boost;
 
-
-use std::io::prelude::*;
-use std::fs::File;
 use std::env;
 
-use boost::data_type::*;
-use boost::booster::core::Booster;
-use boost::booster::adaboost::AdaBoost;
-use boost::base_learner::dstump::DStump;
+use boost::booster::Booster;
+use boost::booster::{AdaBoost, LPBoost, ERLPBoost};
+use boost::base_learner::DStump;
 
-use boost::data_reader::read_libsvm;
+use boost::data_reader::{read_libsvm, read_csv};
 
 
-#[test]
-fn boosting_test() {
-    let mut path = env::current_dir().unwrap();
-    println!("path: {:?}", path);
-    path.push("tests/small_toy_example.txt");
-    let mut file = File::open(path).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let mut examples: Vec<Data<f64>> = Vec::new();
-    let mut labels: Vec<Label<f64>> = Vec::new();
+/// Tests for `AdaBoost`.
+#[cfg(test)]
+pub mod adaboost_tests {
+    use super::*;
+    #[test]
+    fn run() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
 
-    for line in contents.lines() {
-        let mut line = line.split_whitespace();
-        let _label = line.next().unwrap();
-        let _example = line.next().unwrap();
-        let _label = _label.trim_end_matches(':').parse::<f64>().unwrap();
-        labels.push(_label);
+        let sample = read_csv(&path).unwrap();
 
-        let _example = _example.split(',').map(|x| x.parse::<f64>().unwrap()).collect();
-        examples.push(Data::Dense(_example));
+
+        let mut adaboost = AdaBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        adaboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for i in 0..sample.len() {
+            let p = adaboost.predict(&sample[i].data);
+            if sample[i].label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!(true);
     }
 
-    let sample = to_sample(examples, labels);
 
-    let mut adaboost = AdaBoost::init(&sample);
-    let dstump = DStump::init(&sample);
-    let dstump = Box::new(dstump);
+    #[test]
+    fn run_with_libsvm() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example_libsvm.txt");
+        let sample = read_libsvm(path).unwrap();
+        println!("sample.len() is: {:?}, sample.feature_len() is: {:?}", sample.len(), sample.feature_len());
 
 
-    adaboost.run(dstump, &sample, 0.1);
+        let mut adaboost = AdaBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
 
 
-    let mut loss = 0.0;
-    for i in 0..sample.len() {
-        let p = adaboost.predict(&sample[i].data);
-        if sample[i].label != p { loss += 1.0; }
+        adaboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for i in 0..sample.len() {
+            let p = adaboost.predict(&sample[i].data);
+            if sample[i].label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!(true);
     }
-
-    loss /= sample.len() as f64;
-    println!("Loss: {}", loss);
-    assert!(true);
 }
 
 
-#[test]
-fn boosting_with_libsvm() {
-    let mut path = env::current_dir().unwrap();
-    println!("path: {:?}", path);
-    path.push("tests/small_toy_example_libsvm.txt");
-    let sample = read_libsvm(path).unwrap();
-    println!("sample.len() is: {:?}", sample.len());
-    println!("sample.feature_len() is: {:?}", sample.feature_len());
+/// Tests for `LPBoost`.
+#[cfg(test)]
+pub mod lpboost_tests {
+    use super::*;
+    #[test]
+    fn run() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
+        let sample = read_csv(&path).unwrap();
+    
+        let mut lpboost = LPBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
 
 
-    let mut adaboost = AdaBoost::init(&sample);
-    let dstump = DStump::init(&sample);
-    let dstump = Box::new(dstump);
+        lpboost.run(dstump, &sample, 0.1);
 
 
-    adaboost.run(dstump, &sample, 0.1);
+        let mut loss = 0.0;
+        for i in 0..sample.len() {
+            let p = lpboost.predict(&sample[i].data);
+            if sample[i].label != p { loss += 1.0; }
+        }
 
-
-    let mut loss = 0.0;
-    for i in 0..sample.len() {
-        let p = adaboost.predict(&sample[i].data);
-        if sample[i].label != p { loss += 1.0; }
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - lpboost.weights.iter().sum::<f64>().abs()) < 1e-9);
     }
 
-    loss /= sample.len() as f64;
-    println!("Loss: {}", loss);
-    assert!(true);
+
+    #[test]
+    fn run_with_libsvm() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example_libsvm.txt");
+        let sample = read_libsvm(path).unwrap();
+        println!("sample.len() is: {:?}, sample.feature_len() is: {:?}", sample.len(), sample.feature_len());
+
+
+        let mut lpboost = LPBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        lpboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for i in 0..sample.len() {
+            let p = lpboost.predict(&sample[i].data);
+            if sample[i].label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - lpboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
 }
+
+
+
+/// Tests for `ERLPBoost`.
+#[cfg(test)]
+pub mod erlpboost_tests {
+    use super::*;
+    #[test]
+    fn run() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example.csv");
+        let sample = read_csv(&path).unwrap();
+    
+        let mut erlpboost = ERLPBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        erlpboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for i in 0..sample.len() {
+            let p = erlpboost.predict(&sample[i].data);
+            if sample[i].label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - erlpboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+
+
+    #[test]
+    fn run_with_libsvm() {
+        let mut path = env::current_dir().unwrap();
+        println!("path: {:?}", path);
+        path.push("tests/small_toy_example_libsvm.txt");
+        let sample = read_libsvm(path).unwrap();
+        println!("sample.len() is: {:?}, sample.feature_len() is: {:?}", sample.len(), sample.feature_len());
+
+
+        let mut erlpboost = ERLPBoost::init(&sample);
+        let dstump = DStump::init(&sample);
+        let dstump = Box::new(dstump);
+
+
+        erlpboost.run(dstump, &sample, 0.1);
+
+
+        let mut loss = 0.0;
+        for i in 0..sample.len() {
+            let p = erlpboost.predict(&sample[i].data);
+            if sample[i].label != p { loss += 1.0; }
+        }
+
+        loss /= sample.len() as f64;
+        println!("Loss: {}", loss);
+        assert!((1.0 - erlpboost.weights.iter().sum::<f64>().abs()) < 1e-9);
+    }
+}
+
+
